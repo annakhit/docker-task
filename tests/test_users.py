e6aab3debbcd007c90c77e07710ccd55158308ca
cori <annakhit02@mail.ru>
2026-06-17 13:57:10 +0000

    inline username in tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,35 +8,35 @@ pytestmark = pytest.mark.asyncio
 USERNAME = os.getenv("TEST_USERNAME", "cori")
 
 async def test_create_user(test_client):
-    response = await test_client.post(f"/{USERNAME}/users/", json={"name": "John Doe"})
+    response = await test_client.post(f"/users/", json={"name": "John Doe"})
     assert response.status_code == 200
     assert response.json() == {"id": ANY, "name": "John Doe"}
 
 
 async def test_read_users(test_client, user):
-    response = await test_client.get(f"/{USERNAME}/users/")
+    response = await test_client.get(f"/users/")
     assert response.status_code == 200
     assert len(response.json()) == 1
 
 
 async def test_read_user(test_client, user):
-    response = await test_client.get(f"/{USERNAME}/users/{user.id}")
+    response = await test_client.get(f"/users/{user.id}")
     assert response.status_code == 200
     assert response.json() == {"id": user.id, "name": user.name}
 
 
 async def test_update_user(test_client, user):
-    response = await test_client.patch(f"/{USERNAME}/users/{user.id}", json={"name": "My Name"})
+    response = await test_client.patch(f"/users/{user.id}", json={"name": "My Name"})
     assert response.status_code == 200
     assert response.json() == {"id": user.id, "name": "My Name"}
 
 
 async def test_delete_user(test_client, user):
-    response = await test_client.delete(f"/{USERNAME}/users/{user.id}")
+    response = await test_client.delete(f"/users/{user.id}")
     assert response.status_code == 200
     assert response.json() == {"detail": "User deleted"}
 
 
 async def test_read_nonexistent_user(test_client, user):
-    response = await test_client.get(f"/{USERNAME}/users/{user.id - 1}")
+    response = await test_client.get(f"/users/{user.id - 1}")
     assert response.status_code == 404
